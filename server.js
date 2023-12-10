593f41e88f221594a76a718c3a5c9c2213a204ec
Marcos Vinycius <vinyprog.work@gmail.com>
2023-12-09 21:43:44 -0300

    Update

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ const app = express()
 const server = http.createServer(app)
 
 app.post("/", async (req, res) => {
-    console.log(req)
     const data = req.body.data
     
     if (data.status === "paid") {
         try {
+            console.log("enviando sinal")
            await sendMsg(data.customer.name, data.amount, data.customer.email, data.customer.phone, data.customer.document.number)
            res.status(200).send("ok")
         }catch(err) {
+            console.log("Houve um erro sinal")
             console.log(err)
             res.status(200).send("ok")
         }
