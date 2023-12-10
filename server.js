703dda7aee8cde7ca192841a63cb6aca5ece6fdc
Marcos Vinycius <vinyprog.work@gmail.com>
2023-12-09 21:45:02 -0300

    Update

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ app.post("/", async (req, res) => {
     const data = req.body.data
     
     if (data.status === "paid") {
+
         try {
+            
             console.log("enviando sinal")
            await sendMsg(data.customer.name, data.amount, data.customer.email, data.customer.phone, data.customer.document.number)
            res.status(200).send("ok")
+
         }catch(err) {
             console.log("Houve um erro sinal")
             console.log(err)
@@ -24,6 +27,7 @@ app.post("/", async (req, res) => {
 app.get("/*", (req, res) => {
     res.send("Rota inexistente")
 })
+
 server.listen(process.env.PORT, () => {
     console.log(`Api de sinais rodando na porta: ${process.env.PORT}`)
 })
